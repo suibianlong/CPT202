ea32293110a1f830cc7bbda9f357efe2c3bb729c
Yuuu-05 <xxx050920@qq.com>
2026-04-13 17:01:53 +0800

    Revert "Add module3 project"
    This reverts commit 1139450881dd98de0ab72c580a752c8de391b99c.

--- src/main/java/com/cpt202/module3/service/TagService.java
+++
@@ -1,9 +0,0 @@
-package com.cpt202.module3.service;
-
-import com.cpt202.module3.vo.CategoryTagOptionVO;
-import java.util.List;
-
-public interface TagService {
-
-    List<CategoryTagOptionVO> listTagOptions();
-}
--- src/main/java/com/cpt202/module3/util/SessionKeys.java
+++
@@ -1,9 +0,0 @@
-package com.cpt202.module3.util;
-
-public final class SessionKeys {
-
-    public static final String USER_ID = "userId";
-
-    private SessionKeys() {
-    }
-}
